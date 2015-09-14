3614e5a101396398645eb950fc7ab6bbac351439
rtrjl <rt@zone42.fr>
2015-09-14 16:46:02 +0200

    ajout page login

---
+++ app/routes/login.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    beforeModel: function () {
+
+    },
+    activate: function () {
+        Ember.$('body').toggleClass("login-page")
+    },
+    deactivate: function () {
+        Ember.$('body').toggleClass("login-page")
+    }
+
+});
