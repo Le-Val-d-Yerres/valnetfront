acae7d1b87d57b2e95758dc0b406b8f406b353ba
rtrjl <rt@zone42.fr>
2016-02-03 17:05:20 +0100

    suite dev

---
+++ app/routes/poserconge.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  activate: function () {
+        Ember.$('body').toggleClass("skin-blue");
+        Ember.$('body').toggleClass("sidebar-mini");
+        Ember.$('body').toggleClass("wysihtml5-supported");
+    },
+    deactivate: function () {
+        Ember.$('body').toggleClass("skin-blue");
+        Ember.$('body').toggleClass("sidebar-mini");
+        Ember.$('body').toggleClass("wysihtml5-supported");
+    },
+});
